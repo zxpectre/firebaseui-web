f0392691b6d0c66989c737c87c7e828bcf106e52
zxpectre <15870846+zxpectre@users.noreply.github.com>
2021-03-25 22:16:27 -0300

    Final working version

--- build-es.sh
+++ build-es.sh
@@ -3,4 +3,4 @@
 # if ChromeDriver fails with “DevToolsActivePort file doesn't exist”, 
 # add "--remote-debugging-port=9222" in  protractor.conf.js
 
-npm install && npm run build build-js-es-419 && npm run build build-npm-es-419 && npm run build build-esm-es-419
+npm install && npm run build build-js-es-419 && npm run build build-npm-es-419 && npm run build build-esm-es-419 && npm run prepublish && npm publish .
